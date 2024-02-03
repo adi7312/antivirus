a9f64502e8a042e0f6cc53291d662242b8cb218a
ad1s0n <zdrian.alewski@gmail.com>
2024-02-03 01:44:44 +0100

    feat: finding if signature exists in db

--- src/avdaemon.c
+++ src/avdaemon.c
@@ -22,6 +22,7 @@ int main(int argc, char const *argv[])
 {
     sqlite3 *db;
     connect_db(&db);
+    
     return 0;
 }
 
@@ -55,4 +56,22 @@ char* compute_md5(const char *filename)
     }
     hash_string[2* MD5_DIGEST_LENGTH] = '\0';
     return hash_string;
+}
+
+int find_signature_in_db(const char* hashstring, sqlite3 **db){
+    const char* sql;
+    sqlite3_stmt *res;
+    sql = "SELECT * FROM SIGNATURES WHERE MD5_SIGNATURE=?";
+    int rc = sqlite3_prepare_v2(db, sql, -1, &res,0);
+    if (rc == SQLITE_OK){
+        sqlite3_bind_text(res, 1, hashstring, -1, SQLITE_STATIC);
+    } else{
+        fprintf(stderr, "Failed to execute SQL statement: %s\n",sqlite3_errmsg(db));
+    }
+    int step = sqlite3_step(res);
+    sqlite3_finalize(res);
+    if (step == SQLITE_ROW){
+        return 0;
+    }
+    return 1;
 }
--- src/avdaemon.h
+++ src/avdaemon.h
@@ -3,3 +3,4 @@
 #include <openssl/md5.h>
 
 char* compute_md5(const char* filename);
+int find_signature_in_db(const char* hashstring, sqlite3 **db);
